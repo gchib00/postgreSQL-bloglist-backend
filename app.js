964c4cff5b72f18614a2b62791aaaf3ada699818
Giorgi Chibukhashvili <chibukhashviligiorgi@gmail.com>
2022-01-19 22:22:27 +0100

    completed exercise 13.3

--- app.js
+++ app.js
@@ -11,22 +11,16 @@ const mongoose = require('mongoose')
 
 app.use(cors())
 app.use(express.json())
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
-  })
+
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 // eslint-disable-next-line no-undef
-if (process.env.NODE_ENV === 'test') {
-  const testingRouter = require('./controllers/testing')
-  app.use('/api/testing', testingRouter)
-}
-  
+// if (process.env.NODE_ENV === 'test') {
+//   const testingRouter = require('./controllers/testing')
+//   app.use('/api/testing', testingRouter)
+// }
+
 
 module.exports = app
